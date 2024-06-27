6cbc9a6772ff944359ca11e829614f92a13a2cdf
Jason Hargrove <rijaha@gmail.com>
2024-06-27 09:20:31 -0500

    My first commit

---
+++ unesco_go_seek/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unesco_go_seek project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unesco_go_seek.settings")
+
+application = get_asgi_application()
