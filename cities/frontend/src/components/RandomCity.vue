e783bbccd45488c2ff1a8788f9270e33e2096122
Jason Hargrove <rijaha@gmail.com>
2024-06-29 13:54:24 -0500

    Created a Vue component
    Created a component for displaying radom cities.

---
+++ cities/frontend/src/components/RandomCity.vue
@@ -0,0 +1,37 @@
+<
+<template>
+  <div class="random-city">
+    <button @click="fetchRandomCity" class="btn btn-primary">
+      Get Random City
+    </button>
+    <div v-if="city">
+      <h3>{{ city.name }}</h3>
+      <p>{{ city.country }}</p>
+      <p>{{ city.specialization }}</p>
+    </div>
+  </div>
+</template>
+>
+
+<script>
+export default {
+  data() {
+    return {
+      city: null,
+    }
+  },
+  methods: {
+    async fetchRandomCity() {
+      const response = await fetch('/cities/random/')
+      this.city = await response.json()
+    },
+  },
+}
+</script>
+
+<style scoped>
+.random-city {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
