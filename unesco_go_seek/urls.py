169ffdb82655f40d0352c83bc7471a79a4ae2eb1
Jason Hargrove <rijaha@gmail.com>
2024-07-01 20:29:50 -0500

    Working on the button functionality

--- unesco_go_seek/urls.py
+++ unesco_go_seek/urls.py
@@ -22,5 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cities.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
